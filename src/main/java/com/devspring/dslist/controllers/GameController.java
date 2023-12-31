29edd6fee30977306507798f660d4d3099f0606c
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-12-31 14:07:32 -0300

    GET by id

--- src/main/java/com/devspring/dslist/controllers/GameController.java
+++ src/main/java/com/devspring/dslist/controllers/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devspring.dslist.dto.GameDTO;
 import com.devspring.dslist.dto.GameMinDTO;
 import com.devspring.dslist.services.GameService;
 
@@ -24,4 +26,9 @@ public class GameController {
 		 return ResponseEntity.ok().body(list);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GameDTO> findAll(@PathVariable Long id) {
+		 GameDTO game = service.findById(id);
+		 return ResponseEntity.ok().body(game);
+	}
 }
--- src/main/java/com/devspring/dslist/services/GameService.java
+++ src/main/java/com/devspring/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.devspring.dslist.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devspring.dslist.dto.GameDTO;
 import com.devspring.dslist.dto.GameMinDTO;
 import com.devspring.dslist.entities.Game;
 import com.devspring.dslist.repositories.GameRepository;
@@ -20,6 +23,16 @@ public class GameService {
 		
 		// Transforma uma lista de Game em uma GameMinDTO
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Optional<Game> result = gameRepository.findById(id);
+		
+		// Transforma uma lista de Game em uma GameMinDTO
+		GameDTO dto = new GameDTO(result.get());
 		
 		return dto;
 	}
